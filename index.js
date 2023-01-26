68ecbc98a2ccebd9a511173ce7155a72fd250df8
Rajaram manikanta <rajarammmanikantamani@gmail.com>
2023-01-26 18:06:52 +0000

    Super Over League tests Solution

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div class="bg-container">
+    <h1 class="main-heading">Super Over League</h1>
+    <div class="imagecontainer">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="image"
+      />
+    </div>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
